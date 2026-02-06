f7edd0a5fbf94284ece7c19b8be28dfd01d4a092
DAvornu8 <desiree.avornu26@compscihigh.org>
2026-02-06 20:22:10 +0000

    Pushing code

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const inventory = [
 ]
 
 // Task 1: Set the view engine to EJS. 
-
+app.set("view engine", "ejs")
 
 
 app.use((req, res, next) => {
@@ -74,15 +74,19 @@ app.use((req, res, next) => {
 
 // Task 1: Set up the static middleware
 
-
+app.use(express.static(__dirname + "/public"))
 
 // Task 2: Set up the route handler for / to send back the index.html file
 
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // Task 3: Set up the route handler for /mens which sends back category.ejs with the men's category object
 
-
+app.get("/mens",(req, res) => {
+  res.render("category.ejs", inventory[0]);
+});
 
 // Task 4: Plug in the values in category.ejs to get the page working
 // PINK ONLY: Set up a route handler for /womens to pass in similar data for women's
